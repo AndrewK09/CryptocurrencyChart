11132c2fc89146912b653f989f2cd8d58b6ba28c
Andrew <Andrewkn09@Gmail.com>
2019-07-10 13:10:50 -0400

    Add graph

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,74 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: []
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              type: 'time',
+              ticks: {
+                autoSkip: true,
+                maxTicksLimit: 12
+              }
+            }
+          ]
+        }
+      }
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/yearly').then(({ data }) => {
+      let dates = Object.keys(data.bpi);
+      // let filtered = dates.map(date => date.substring(5));
+
+      let values = Object.values(data.bpi);
+      let newData = Object.assign({}, this.state.data);
+      newData.labels = dates;
+      newData.datasets[0].data = values;
+
+      this.setState({
+        data: newData
+      });
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>hi there</h2>
+        <Line data={this.state.data} options={this.state.options} />
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -6,6 +6,42 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+const axios = require('axios');
+
+app.get('/currentPrice', (req, res) => {
+  axios
+    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(result => {
+      res.send(result.data);
+    });
+});
+
+app.get('/daily', (req, res) => {
+  axios
+    .get('https://api.coindesk.com/v1/bpi/historical/close.json?index=[USD]')
+    .then(result => {
+      res.send(result.data);
+    });
+});
+
+app.get('/yesterday', (req, res) => {
+  axios
+    .get('https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday')
+    .then(result => {
+      res.send(result.data);
+    });
+});
+
+app.get('/yearly', (req, res) => {
+  axios
+    .get(
+      'https://api.coindesk.com/v1/bpi/historical/close.json?start=2018-07-10&end=2019-07-10'
+    )
+    .then(result => {
+      res.send(result.data);
+    });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
